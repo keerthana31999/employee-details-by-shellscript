bf993f27fe52acdb6b57e31dc77d59a713864d57
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 22:37:35 +0530

    Added::singlet combination

--- employeDETAIL.sh
+++ employeDETAIL.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -x
-
 coutHeadWin=0
 countTailsWin=0
+declare -A singlet
+function percentagesinglet(){
+	percentageHead=$(( (${singlet[H]}/20)*100 ))
+	echo "H-" $percentageHead
+	percentagetails=$(( (${singlet[H]}/20)*100 ))
+	echo "T-" $percentagetails
+
+}
 for (( i=1; i<=20; i++ ))
 do
 	flip=$((RANDOM%2))
@@ -9,10 +16,14 @@ do
 	then
 		echo "head won"
 		((countHeadWin++))
+		singlet[H]=$countHeadWin
 	else
 		echo "tails won"
 		((countTailsWin++))
+		singlet[T]=$countTailsWin
 	fi
 done
 echo "head win count out of 20 is=" $countHeadWin
 echo "tails win count out of 20 is=" $countTailsWin
+echo "[ ${singlet[@]} ]"
+percentagesinglet
