55ceea0d29a886cb6825b4bfa6c596fd3f4a1a55
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 23:02:39 +0530

    Added::double combination

--- employeDETAIL.sh
+++ employeDETAIL.sh
@@ -9,21 +9,71 @@ function percentagesinglet(){
 	echo "T-" $percentagetails
 
 }
+function percentageDoublet(){
+        percentageHH=$(( (${doublet[HH]}/20)*100 ))
+        echo "HH-" $percentageHH
+
+        percentageHT=$(( (${doublet[HT]}/20)*100 ))
+        echo "HT-" $percentageHT
+
+	percentageTT=$(( (${doublet[TT]}/20)*100 ))
+        echo "TT-" $percentageHT
+
+	percentageTH=$(( (${doublet[TH]}/20)*100 ))
+        echo "TH-" $percentageTH
+
+}
+
+
 for (( i=1; i<=20; i++ ))
 do
 	flip=$((RANDOM%2))
 	if (( $flip==1 ))
 	then
 		echo "head won"
-		((countHeadWin++))
-		singlet[H]=$countHeadWin
+		((coutHeadWin++))
+		 singlet[H]=$coutHeadWin
 	else
 		echo "tails won"
 		((countTailsWin++))
 		singlet[T]=$countTailsWin
 	fi
 done
-echo "head win count out of 20 is=" $countHeadWin
-echo "tails win count out of 20 is=" $countTailsWin
+
+for (( i=1; i<=20; i++ ))
+do
+        flip1=$((RANDOM%2))
+	flip2=$((RANDOM%2))
+        if (( $flip1==1 && $flip2==1 ))
+        then
+                echo "HH"
+                ((headHead++))
+                doublet[HH]=$headHead
+        elif (( $flip1==1 && $flip2==0 ))
+	then
+                echo "HT"
+                ((headTail++))
+                doublet[HT]=$headTail
+	 elif (( $flip1==0 && $flip2==0 ))
+	 then
+                echo "TT"
+                ((tailTail++))
+                doublet[TT]=$tailTail
+	elif (( $flip1==0 && $flip2==1 ))
+         then
+                echo "TH"
+                ((tailHead++))
+                doublet[TH]=$tailHead
+	else
+		echo "error"
+
+        fi
+done
+
 echo "[ ${singlet[@]} ]"
-percentagesinglet
+echo "[ ${!singlet[@]} ]"
+echo "[ ${doublet[@]} ]"
+echo "[ ${!doublet[@]} ]"
+
+percentageSinglet
+percentageDoublet
